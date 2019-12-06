22af738544feca42cf005743bda3cd273b5835ad
hamzaoui aymen <elhamzaouiaymen@gmail.com>
2019-12-06 23:57:19 +0100

    job list issue fixed

--- coding-lab-ng-poc/src/app/modules/jobs/jobs-list/jobs-list.component.ts
+++ coding-lab-ng-poc/src/app/modules/jobs/jobs-list/jobs-list.component.ts
@@ -7,14 +7,6 @@ import { Job } from '../../../models/job.model';
   styleUrls: ['./jobs-list.component.sass']
 })
 export class JobsListComponent implements OnInit {
-
-  jobs: Job [] = [
-    new Job(1,"A French Translator needed", "We are hiring a translator for our website content"),
-    new Job(2,"A French Translator needed", "We are hiring a translator for our website content"),
-    new Job(3,"A French Translator needed", "We are hiring a translator for our website content"),
-    new Job(4,"A French Translator needed", "We are hiring a translator for our website content"),
-    new Job(5,"A French Translator needed", "We are hiring a translator for our website content"),
-  ]
   constructor() { }
 
   ngOnInit() {
