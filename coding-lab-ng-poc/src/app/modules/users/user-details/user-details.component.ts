95aabddd729dd1fa337544743f57d7dfde851d48
hamzaoui aymen <elhamzaouiaymen@gmail.com>
2019-12-08 19:26:11 +0100

    user details minor changes

--- coding-lab-ng-poc/src/app/modules/users/user-details/user-details.component.ts
+++ coding-lab-ng-poc/src/app/modules/users/user-details/user-details.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { User } from '../../../models/user.model';
+import { NgxSpinnerService } from 'ngx-spinner';
+import { ActivatedRoute } from '@angular/router';
+import { EntityService } from '../../../services/entity.service';
+import { HttpErrorResponse } from '@angular/common/http';
 
 @Component({
   selector: 'app-user-details',
@@ -6,10 +12,50 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user-details.component.sass']
 })
 export class UserDetailsComponent implements OnInit {
+  user: User
+  userId: number
+  activatedRouteSubscription: Subscription
+  fetchCurrentUserSubscription: Subscription
 
-  constructor() { }
+  constructor(private spinnerService: NgxSpinnerService,
+    private activatedRoute: ActivatedRoute,
+    private backendService: EntityService<User>) {
+
+      this.activatedRouteSubscription =  this.activatedRoute.params.subscribe((params: any) => {
+        this.userId = params['id']
+      })
+
+    }
 
   ngOnInit() {
+    this.fetchCurrentUser()
+  }
+
+  ngOnDestroy(){
+  if(this.fetchCurrentUserSubscription !== undefined) this.fetchCurrentUserSubscription.unsubscribe()
+  if(this.activatedRouteSubscription !== undefined) this.activatedRouteSubscription.unsubscribe()
+  }
+
+  fetchCurrentUser(){
+    this.showLoader();
+    if(this.userId !== undefined){
+      this.fetchCurrentUserSubscription = this.backendService.findById(User, this.userId).subscribe((user: User) => {
+      this.user = user
+      this.hideLoader()
+    }, (error: HttpErrorResponse) => {
+      console.log(error.message)
+    })
+    }
+  }
+
+  showLoader(){
+  this.spinnerService.show()
+  }
+
+  hideLoader(){
+  setTimeout(() => {
+  this.spinnerService.hide()
+  }, 500);
   }
 
 }
